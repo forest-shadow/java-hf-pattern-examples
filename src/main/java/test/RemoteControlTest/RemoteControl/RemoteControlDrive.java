e62675c5a1ac40013ae777f0268b53535267e24c
forest-shadow <mamasay@yandex.ru>
2020-02-18 13:27:40 +0300

    Prettify file

--- src/main/java/test/RemoteControlTest/RemoteControl/RemoteControlDrive.java
+++ src/main/java/test/RemoteControlTest/RemoteControl/RemoteControlDrive.java
@@ -14,27 +14,18 @@ public class RemoteControlDrive {
     Stereo stereo = new Stereo("Living Room");
 
     LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
-    LightOffCommand livingRoomLightOff =
-  new LightOffCommand(livingRoomLight);
-    LightOnCommand kitchenLightOn =
-  new LightOnCommand(kitchenLight);
-    LightOffCommand kitchenLightOff =
-  new LightOffCommand(kitchenLight);
+    LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+    LightOnCommand kitchenLightOn = new LightOnCommand(kitchenLight);
+    LightOffCommand kitchenLightOff = new LightOffCommand(kitchenLight);
 
-    CeilingFanOnCommand ceilingFanOn =
-  new CeilingFanOnCommand(ceilingFan);
-    CeilingFanOffCommand ceilingFanOff =
-  new CeilingFanOffCommand(ceilingFan);
+    CeilingFanOnCommand ceilingFanOn = new CeilingFanOnCommand(ceilingFan);
+    CeilingFanOffCommand ceilingFanOff = new CeilingFanOffCommand(ceilingFan);
     
-    GarageDoorUpCommand garageDoorUp =
-  new GarageDoorUpCommand(garageDoor);
-    GarageDoorDownCommand garageDoorDown =
-  new GarageDoorDownCommand(garageDoor);
+    GarageDoorUpCommand garageDoorUp = new GarageDoorUpCommand(garageDoor);
+    GarageDoorDownCommand garageDoorDown = new GarageDoorDownCommand(garageDoor);
 
-    StereoOnWithCDCommand stereoOnWithCD =
-  new StereoOnWithCDCommand(stereo);
-    StereoOffCommand stereoOff =
-  new StereoOffCommand(stereo);
+    StereoOnWithCDCommand stereoOnWithCD = new StereoOnWithCDCommand(stereo);
+    StereoOffCommand stereoOff = new StereoOffCommand(stereo);
 
     remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
     remoteControl.setCommand(1, kitchenLightOn, kitchenLightOff);
